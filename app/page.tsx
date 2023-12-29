c96271e9ba628489deeae8e519d2676d179555de
rivered <rivered@rivered.net>
2023-12-30 03:16:40 +0900

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,55 @@
-import Image from 'next/image'
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button
-        className='btn btn-primary'
-      >ボタン</button>
-    </main>
+    <body
+      className=""
+    >
+
+      <main className="flex min-h-screen flex-col justify-between p-24">
+        {/* タイムライン */}
+        <div
+          className=""
+        >
+          <div
+            className=" w-auto"
+          >
+            <div
+              className="bg-primary p-3 rounded-lg"
+            >
+              <p>Name</p>
+              <p>message...message...message...message...message...message...message...</p>
+            </div>
+            <p
+              className=" text-right"
+            >yyyy-mm-dd hh:mm:ss</p>
+          </div>
+
+
+        </div>
+        {/*  */}
+        <div
+          className="flex gap-3 w-full"
+        >
+          <div
+            className="join w-full"
+          >
+            <input type="text"
+              className="input input-bordered join-item w-full"
+              placeholder="input message..."
+            />
+            <button
+              className='btn btn-primary join-item'
+            >投稿</button>
+          </div>
+          <button
+            className="btn btn-secondary"
+          >
+            なんかボタン
+          </button>
+        </div>
+      </main>
+    </body>
   )
 }
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-export type Database = {};
+export type Database = {
+    id: string;
+    name: string;
+    message: string;
+    created_at: string;
+    is_ai: boolean;
+};
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
